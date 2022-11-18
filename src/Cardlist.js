923f1c95a912e0c55bc91a81f9bdd91350b1e8bc
Tukang Logika <yanto.heri@outlook.com>
2022-11-18 19:40:06 +0700

    with search box & style

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -18,20 +18,25 @@ const Cardlist = ({robots}) => {
     //     />)
     // })
 
-    return (
-        <Fragment>
-            {robots.map ( (user,i) => {
-                return (<Card 
-                        key={`id${i}`} 
-                        id={robots[i].id} 
-                        name={robots[i].fullName} 
-                        email = {robots[i].email} 
-                        />);
-               } 
-               )
-            }
-        </Fragment>
-    );
+    if (robots.length === 0) {
+        return <h1 className="tc">Loading</h1>
+    } else {
+        return (
+            <Fragment>
+                {robots.map ( (user,i) => {
+                    return (<Card 
+                            key={`id${i}`} 
+                            id={robots[i].id} 
+                            name={robots[i].name} 
+                            email = {robots[i].email} 
+                            />);
+                   } 
+                   )
+                }
+            </Fragment>
+        );
+    }
+    
 
 }
 
